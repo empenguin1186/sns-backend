4c0e5577a422064156b43b201824deda84da363d
kodaira <kodaira@yahoo-corp.jp>
2019-02-26 10:08:04 +0900

    🐧 UserクラスのbirthDayフィールドをLocalDateクラスに変更

--- src/main/java/jp/co/training/sns/SnsApplication.java
+++ src/main/java/jp/co/training/sns/SnsApplication.java
@@ -1,10 +1,7 @@
 package jp.co.training.sns;
 
-import jp.co.training.sns.Mapper.UserMapper;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -14,17 +11,6 @@ public class SnsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SnsApplication.class, args);
 	}
-	
-//	private final UserMapper userMapper;
-//
-//	public SnsApplication(UserMapper userMapper) {
-//		this.userMapper = userMapper; // Mapperをインジェクションする
-//	}
-//
-//	@Bean
-//	CommandLineRunner sampleCommandLineRunner() {
-//		return (args) -> System.out.println(this.userMapper.findOne("kodaira"));
-//	}
 
 }
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO user (user_id, user_name, encoded_password, email, comment, place, birth_day, img_url) VALUES ('kodaira', 'kodaira', '$2a$10$zFEweFHkCxWQSxOXmO.Rn.IlNeXuyHE8DxLLtPKSntbCXF3ZBLG96', 'sample.yahoo.co.jp', 'hoge', 'Tokyo', 100000, 'example.com');
-INSERT INTO user (user_id, user_name, encoded_password, email, comment, place, birth_day, img_url) VALUES ('hoge', 'hoge', '$2a$10$zFEweFHkCxWQSxOXmO.Rn.IlNeXuyHE8DxLLtPKSntbCXF3ZBLG96', 'sample.yahoo.co.jp', 'hoge', 'Tokyo', 100000, 'example.com');
+INSERT INTO user (user_id, user_name, encoded_password, email, comment, place, birth_day, img_url) VALUES ('kodaira', 'kodaira', '$2a$10$zFEweFHkCxWQSxOXmO.Rn.IlNeXuyHE8DxLLtPKSntbCXF3ZBLG96', 'sample.yahoo.co.jp', 'hoge', 'Tokyo', '2000-01-01', 'example.com');
+INSERT INTO user (user_id, user_name, encoded_password, email, comment, place, birth_day, img_url) VALUES ('hoge', 'hoge', '$2a$10$zFEweFHkCxWQSxOXmO.Rn.IlNeXuyHE8DxLLtPKSntbCXF3ZBLG96', 'sample.yahoo.co.jp', 'hoge', 'Tokyo', '2000-01-02', 'example.com');
 INSERT INTO follow (follow_id, follower_id, block_flag, mute_flag) VALUES ('kodaira', 'hoge', 1, 0);
 INSERT INTO tweet (content, tweet_user, attachment, favorite, retweet, reply_to, time) VALUES ('test message', 'kodaira', 'attachment', 0, 0, 1, 300);
 INSERT INTO tweet (content, tweet_user, attachment, favorite, retweet, reply_to, time) VALUES ('アイウエオかきくけこさしすせそたちつてとなにぬねのは皮膚へ保まみむめもやゆよらりるれろわをん', 'hoge', 'attachment', 0, 0, 1, 300);
