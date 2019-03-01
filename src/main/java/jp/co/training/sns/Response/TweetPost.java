f390d0dc4a0f169d28bd685ae7b1480a803e8c91
kodaira <kodaira@yahoo-corp.jp>
2019-03-01 10:59:56 +0900

    🐧 JSONレスポンスのプロパティ名を変更

--- src/main/java/jp/co/training/sns/Response/TweetPost.java
+++ src/main/java/jp/co/training/sns/Response/TweetPost.java
@@ -1,5 +1,6 @@
 package jp.co.training.sns.Response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -8,7 +9,9 @@ import java.io.Serializable;
 @Data
 @AllArgsConstructor
 public class TweetPost implements Serializable {
+
+	@JsonProperty("status_code")
 	private int statusCode;
-	
+
 	private String message;
 }
